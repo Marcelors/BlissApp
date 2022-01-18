9655e9f7b2d3e3566d15f09c6b171b4580ff0e3d
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2022-01-18 12:02:55 -0300

    feat: create question and choice entities

---
+++ src/Domain/Entities/Choices.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Choices : Entity
+    {
+        public string Choice { get; private set; }
+        public int Votes { get; private set; }
+
+        private Choices() { }
+
+        public Choices(string choice)
+        {
+            Choice = choice;
+            Votes = 0;
+        }
+    }
+}
---
+++ src/Domain/Entities/Questions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Questions : Entity
+    {
+        public string ImageUrl { get; private set; }
+        public string ThumbUrl { get; private set; }
+        public string Question { get; private set; }
+        public DateTime PublishedAt { get; private set; }
+        public List<Choices> Choices { get; set; } = new();
+
+        private Questions() { }
+
+        public Questions(string question, string imageUrl, string thumbUrl)
+        {
+            Question = question;
+            ImageUrl = imageUrl;
+            ThumbUrl = thumbUrl;
+            PublishedAt = DateTime.UtcNow;
+        }
+    }
+}

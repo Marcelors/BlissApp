2b1f92a1d710dc1ef36052b53bbf68b6263be08d
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2022-01-18 15:30:38 -0300

    refactor: change method for async

--- src/Domain/Repositories/IQuestionRepository.cs
+++ src/Domain/Repositories/IQuestionRepository.cs
@@ -1,11 +1,12 @@
-﻿using Domain.Entities;
+﻿using System.Threading.Tasks;
+using Domain.Entities;
 
 namespace Domain.Repositories
 {
     public interface IQuestionRepository
     {
-        public void Add(Questions question);
-        public void Update(Questions question);
-        public Questions GetById(int id);
+        public Task Add(Questions question);
+        public Task Update(Questions question);
+        public Task<Questions> GetById(int id);
     }
 }

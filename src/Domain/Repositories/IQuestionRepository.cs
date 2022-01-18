fa9c9fbdead122fbb5ff9ba49825a5c12e7efff2
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2022-01-18 15:05:29 -0300

    feat: add question repository interface

---
+++ src/Domain/Repositories/IQuestionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IQuestionRepository
+    {
+        public void Add(Questions question);
+        public void Update(Questions question);
+        public Questions GetById(int id);
+    }
+}

b7d95ba55ec19ea59990f76402f0dfc775c94e0d
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2022-01-18 21:10:52 -0300

    feat: add question service

---
+++ src/Application/IQuestionService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Application.DTOs;
+
+namespace Application
+{
+    public interface IQuestionService
+    {
+        public Task<QuestionResponseDto> Add(QuestionCreatedRequestDto dto);
+        public Task<QuestionUpdatedRequestDto> Update(int id, QuestionUpdatedRequestDto dto);
+        public Task<QuestionResponseDto> GetById(int id);
+    }
+}
---
+++ src/Application/QuestionService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Application.DTOs;
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Application
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public async Task<QuestionResponseDto> Add(QuestionCreatedRequestDto dto)
+        {
+            var question = new Questions(dto.Question, dto.ImageUrl, dto.ThumbUrl);
+            question.AddChoices(dto.Choices);
+
+            await _questionRepository.Add(question);
+
+            return question;
+        }
+
+        public async Task<QuestionResponseDto> GetById(int id)
+        {
+            var question = await _questionRepository.GetById(id);
+            return question;
+        }
+
+        public Task<QuestionUpdatedRequestDto> Update(int id, QuestionUpdatedRequestDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

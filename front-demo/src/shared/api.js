05d6ba4d7fd5d9aebdf19ff9786f55df0624ea4d
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2022-01-22 11:37:32 -0300

    feat: create service for connect with api

---
+++ front-demo/src/shared/api.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const urlBase = "http://localhost:5000/"
+
+const instance = axios.create({
+    timeout: 3000,
+    headers: { 'Content-Type': 'application/json' },
+});
+
+const getQuestions = (data) => {
+    return instance.get(`${urlBase}questions`, data).then(result => {
+        return result.data;
+    }).catch(error => {
+        throw error.response.data.message
+    });
+}
+
+const getQuestionsById = (id) => {
+    return instance.get(`${urlBase}questions/${id}`).then(result => {
+        return result.data;
+    }).catch(error => {
+        throw error.response.data.message
+    });
+}
+
+const addQuestions = (data) => {
+    return instance.post(`${urlBase}questions`, data).then(result => {
+        return result.data;
+    }).catch(error => {
+        throw error.response.data.message
+    });
+}
+
+const updateQuestions = (id, data) => {
+    return instance.post(`${urlBase}questions/${id}`, data).then(result => {
+        return result.data;
+    }).catch(error => {
+        throw error.response.data.message
+    });
+}
+
+export {
+    getQuestions,
+    getQuestionsById,
+    addQuestions,
+    updateQuestions
+}
